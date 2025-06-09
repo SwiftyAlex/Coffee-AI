6f703ab0676fc50200e0707c01828d3388fdf2ac
SwiftyAlex <me@alexanderlogan.co.uk>
2025-06-10 00:39:37 +0100

    Initial Commit

---
+++ coffeeai/coffeeaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeeaiApp.swift
+//  coffeeai
+//
+//  Created by Alex Logan on 10/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct coffeeaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ coffeeaiTests/coffeeaiTests.swift
@@ -0,0 +1,17 @@
+//
+//  coffeeaiTests.swift
+//  coffeeaiTests
+//
+//  Created by Alex Logan on 10/06/2025.
+//
+
+import Testing
+@testable import coffeeai
+
+struct coffeeaiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
